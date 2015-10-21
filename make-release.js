657c83bd617f83ea98c2eb8d2cdd4e3c85df2fba
Ryan Cavanaugh <searyanc@users.noreply.github.com>
2015-10-20 17:28:40 -0700

    Add script for making releases

---
+++ make-release.js
@@ -0,0 +1,15 @@
+var fs = require('fs');
+
+var fileList = ['data.lua', 'control.lua', 'info.json']
+
+var data = fs.readFileSync('info.json', 'utf-8');
+var parsedData = JSON.parse(data);
+var outDir = 'releases/' + parsedData.name + '_' + parsedData.version;
+
+console.log('Create ' + outDir);
+fs.mkdirSync(outDir);
+fileList.forEach(function(fn) {
+	console.log('Copy ' + fn);
+	fs.createReadStream(fn).pipe(fs.createWriteStream(outDir + '/' + fn));
+});
+console.log('Done!');
